8b449d752f1f7e947745c6474ffc8c4ab2862469
Vaishali Mehmi <89162649+vm799@users.noreply.github.com>
2022-11-27 19:03:22 +0000

    deploying contract

--- cool-domains/scripts/run.js
+++ cool-domains/scripts/run.js
@@ -1,17 +1,20 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
+    // const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
+    const domainContract = await domainContractFactory.deploy("souls");
     await domainContract.deployed();
-    console.log("Contract deployed to:", domainContract.address); 
-    console.log("Contract deployed by:", owner.address); 
 
-    let txn = await domainContract.register("varity");
+    console.log("Contract deployed to:", domainContract.address); 
+    // console.log("Contract deployed by:",address); 
+
+    let txn = await domainContract.register("sinetic", {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
 
-const domainAddress = await domainContract.getAddress("varity");
-console.log("Owner of domain:", domainAddress);
+const address = await domainContract.getAddress("varity");
+console.log("Owner of domain varity:", address);
 
+const balance = await hre.ethers.provider.getBalance(domainContract.address);
+console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 // txn = await domainContract.connect(randomPerson).setRecord("varity", "My domain now! All mine");
 // await txn.wait();
 }
